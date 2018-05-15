814164e76c6ad6bbcd45027869c7e9d35dc80500
dekisa94 <dsimic94@gmail.com>
2018-05-15 19:37:53 +0200

    first commit

---
+++ src/components/AppMovies.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        RAdi
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import AppMovies from './components/AppMovies'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', redirect: 'movies'},
+  {path: '/movies', component: AppMovies}
+]
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
